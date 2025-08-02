06f7deaabb25155d226fdb698717271fc77001f5
bhuwan644 <lak07675@gmail.com>
2025-08-03 10:28:37 +1200

    Updated print function

--- week-1-saturday/rainfall_analysis.py
+++ week-1-saturday/rainfall_analysis.py
@@ -14,12 +14,11 @@ sample_rainfall_array=[0.0, 5.2, 3.1, 0.0, 12.4, 0.0, 7.5]
 sample_rainfall_array_via_numpy=np.array(sample_rainfall_array)
 
 def calculation():
-    print(f"Converted numpy array is: {sample_rainfall_array_via_numpy}")
     total_rainfall=round(np.sum(sample_rainfall_array_via_numpy),1)
     average_rainfall=round(np.mean(sample_rainfall_array_via_numpy))
     no_rain_days = np.sum(sample_rainfall_array_via_numpy== 0.0)
-    rainfall_more_than_5mm=np.where(sample_rainfall_array_via_numpy>5.0)
-    print(f"Total rainfall for the week is : {total_rainfall}\nAverage rainfall for the week is : {average_rainfall}\nTotal no rain days:{no_rain_days}\nTotal days where rainfall was more than 5mm:{rainfall_more_than_5mm}")
+    rainfall_more_than_5mm=np.where(sample_rainfall_array_via_numpy>5.0)[0]
+    print(f"Converted numpy array is: {sample_rainfall_array_via_numpy}\nTotal rainfall for the week is : {total_rainfall}\nAverage rainfall for the week is : {average_rainfall}\nTotal no rain days:{no_rain_days}\nIndices for the days where rainfall was more than 5mm:{rainfall_more_than_5mm}")
 
 
 
@@ -27,5 +26,4 @@ def calculation():
 
 
 if __name__ == "__main__":
-    print("hello")
     calculation()
