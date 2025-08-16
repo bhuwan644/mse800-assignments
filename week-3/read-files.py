cd6bdb8160a3a1ff231715ba316dbdbd791f70fe
bhuwan644 <lak07675@gmail.com>
2025-08-16 12:09:56 +1200

    Added class

--- week-3/read-files.py
+++ week-3/read-files.py
@@ -1,28 +1,33 @@
+class FileMethods:
+    def __init__(self,file_path):
+         self.file_path=file_path
+    
+    def read_file(self):
+        print("Reading a file...................")
+        file=open(self.file_path,"r") # Opens the file in a read mode
+        print("Here is your file")
+        print(file)
+    
+    
+    def write_file(self):
+        print("Writing into file")
+        file=open(self.file_path,"a") # Opens the file in append mode
+        inputted_text=input("Write the text you want to append :")
+        file.write(f"{inputted_text}\n")
+        print("Congrats, your data has been added into the file")
+        file.close()
+    
 
-def read_file():
-    print("Reading a file...................")
-    file=open("demo.txt","r") # Opens the file in a read mode
-    print("Here is your file")
-    print(file)
-    
-    
-def write_file():
-    print("Writing into file")
-    file=open("demo.txt","a") # Opens the file in append mode
-    inputted_text=input("Write the text you want to append :")
-    file.write(f"{inputted_text}\n")
-    print("Congrats, your data has been added into the file")
-    file.close()
-    
 
+file=FileMethods("demo.txt")
 
 def main():
     print("This is the main function")
     method_inserted_by_user=input("Please choose the method, Do you want to read the file or write into it? Press R if you want to read, else press W to write:  ")
     if(method_inserted_by_user.lower()=="r"):
-        read_file()
+        file.read_file()
     else:
-        write_file()
+        file.write_file()
 
 
 
